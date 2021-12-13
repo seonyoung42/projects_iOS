e29da767dcde02026846dfbad94cef966277a85d
seonyoung <4tjsdud2@naver.com>
2021-12-13 18:28:36 +0900

    AutoLayout programmatically

--- QuotesGenerator/QuotesGenerator/AutoLayoutView.swift
+++ QuotesGenerator/QuotesGenerator/AutoLayoutView.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class AutoLayoutView: UIView {
     
+    let quotes = [
+        Quote(contents: "죽음을 두려워하는 나머지 삶을 시작조차 못하는 사람이 많다.", name: "벤다이크"),
+        Quote(contents: "나는 나 자신을 빼 놓고는 모두 안다.", name: "비용"),
+        Quote(contents: "편견이란 실효성이 없는 의견이다.", name: "암브로스 빌"),
+        Quote(contents: "분노는 바보들의 가슴속에서만 살아간다.", name: "아인슈타인"),
+        Quote(contents: "몇 번이라도 좋다! 이 끔찍한 생이여... 다시!", name: "니체")
+    ]
+    
     let titleLabel: UILabel = {
         let label = UILabel()
         
@@ -31,19 +39,21 @@ class AutoLayoutView: UIView {
     let quoteLabel: UILabel = {
         let label = UILabel()
         
-        label.text = "명언 생성기 명언 생성기 명언 생성기 명언 생성기 명언 생성기 명언 생성기명언 생성기 명언 생성기명언 생성기명언 생성기 명언 생성기 명언 생성기 명언 생성기 명언 생성기 명언 생성기 명언 생성기 명언 생성기 명언 생성기 명언 생성기 명언 생성기 명언 생성기명언 생성기 명언 생성기명언 생성기명언 생성기 명언 생성기 명언 생성기 명언 생성기 명언 생성기 명언 생성기 "
+        label.text = ""
         label.translatesAutoresizingMaskIntoConstraints = false
         label.numberOfLines = 0
+        label.textAlignment = .center
 
-        
         return label
     }()
     
     let nameLabel: UILabel = {
         let label = UILabel()
         
-        label.text = "명언가"
+        label.text = ""
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 17, weight: .bold)
 
         return label
     }()
@@ -56,8 +66,20 @@ class AutoLayoutView: UIView {
         button.setTitleColor(.systemBlue, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         
+        button.addTarget(self, action: #selector(tapQuoteButton), for: .touchUpInside)
         return button
     }()
+    
+    
+    @objc func tapQuoteButton() {
+        print("QuoteButton tapped")
+        
+        let random = Int(arc4random_uniform(5)) // 0~4사이의 난수 랜덤하게 생성
+        let quote = quotes[random]
+        
+        self.quoteLabel.text = quote.contents
+        self.nameLabel.text = quote.name
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
