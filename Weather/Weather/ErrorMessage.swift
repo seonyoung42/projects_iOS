e9605eae71cabfac452b0cb8df7e6c5d04f4d0a5
seonyoung <4tjsdud2@naver.com>
2022-01-09 17:27:24 +0900

    URLSession 사용해서 HTTP 통신하기

---
+++ Weather/Weather/ErrorMessage.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorMessage.swift
+//  Weather
+//
+//  Created by 장선영 on 2022/01/09.
+//
+
+import Foundation
+
+struct ErrorMessage: Codable {
+//    let code: String
+    let message: String
+}
--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -85,11 +85,12 @@ class ViewController: UIViewController {
         let stakView = UIStackView()
         stakView.axis = .vertical
         stakView.spacing = 3
+        stakView.alignment = .center
         stakView.translatesAutoresizingMaskIntoConstraints = false
         return stakView
     }()
     
-    let stackView4: UIStackView = {
+    let weatherStackView: UIStackView = {
         let stakView = UIStackView()
         stakView.axis = .vertical
         stakView.alignment = .center
@@ -103,9 +104,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
         view.addSubview(cityNameTextField)
         view.addSubview(button)
-        view.addSubview(stackView4)
+        view.addSubview(weatherStackView)
         
         let safeArea = view.safeAreaLayoutGuide
         cityNameTextField.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 30).isActive = true
@@ -115,12 +117,12 @@ class ViewController: UIViewController {
         button.topAnchor.constraint(equalTo: cityNameTextField.bottomAnchor, constant: 24).isActive = true
         button.centerXAnchor.constraint(equalTo: cityNameTextField.centerXAnchor).isActive = true
         
-        stackView4.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 50).isActive = true
-        stackView4.leadingAnchor.constraint(equalTo: cityNameTextField.leadingAnchor).isActive = true
-        stackView4.trailingAnchor.constraint(equalTo: cityNameTextField.trailingAnchor).isActive = true
+        weatherStackView.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 50).isActive = true
+        weatherStackView.leadingAnchor.constraint(equalTo: cityNameTextField.leadingAnchor).isActive = true
+        weatherStackView.trailingAnchor.constraint(equalTo: cityNameTextField.trailingAnchor).isActive = true
         
-        stackView4.addArrangedSubview(stackView1)
-        stackView4.addArrangedSubview(stackView3)
+        weatherStackView.addArrangedSubview(stackView1)
+        weatherStackView.addArrangedSubview(stackView3)
         
         stackView1.addArrangedSubview(cityNameLabel)
         stackView1.addArrangedSubview(weatherDescriptionLabel)
@@ -131,14 +133,61 @@ class ViewController: UIViewController {
         stackView2.addArrangedSubview(maxTempLabel)
         stackView2.addArrangedSubview(minTempLabel)
         
-        
     }
     
     
     @objc func tapFetchWeatherButton() {
-        
+        if let cityName = self.cityNameTextField.text {
+            getCurrentWeather(cityName: cityName )
+            self.view.endEditing(true)
+        }
+    }
+    
+    
+    func getCurrentWeather(cityName: String) {
+
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=a4f466549aadd32e7ffb064779522389") else { return }
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url) { [weak self] data, response ,error in
+            let successRange = (200..<300)
+
+            guard let data = data, error == nil else {
+                return }
+
+            let decoder = JSONDecoder()
+
+            if let response = response as? HTTPURLResponse, successRange.contains(response.statusCode) {
+
+                guard let weatherInformation = try? decoder.decode(WeatherInformation.self, from: data) else { return }
+                DispatchQueue.main.async {
+                    self?.weatherStackView.isHidden = false
+                    self?.configureView(weatherInformation: weatherInformation)
+                }
+            } else {
+                guard let errorMessage = try? decoder.decode(ErrorMessage.self, from: data) else { return }
+                DispatchQueue.main.async {
+                    self?.showAlert(errorMessage: errorMessage.message)
+                }
+            }
+        }.resume()
     }
 
+    func showAlert(errorMessage: String) {
+        let ac = UIAlertController(title: "Error", message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(ac, animated: true, completion: nil)
+    }
+
+    func configureView(weatherInformation: WeatherInformation) {
+        
+        self.cityNameLabel.text = weatherInformation.name
+        if let weather = weatherInformation.weather.first {
+            self.weatherDescriptionLabel.text = weather.description
+        }
+        self.tempLabel.text = "\(Int(weatherInformation.temp.temp - 273.15)) 도"
+        self.maxTempLabel.text = "최고 \(Int(weatherInformation.temp.maxTemp - 273.15))도"
+        self.minTempLabel.text = "최저 \(Int(weatherInformation.temp.minTemp - 273.15))도"
+    }
 
 }
 
@@ -166,4 +215,5 @@ extension UIViewController {
             Preview(viewController: self)
         }
 }
+
 #endif
