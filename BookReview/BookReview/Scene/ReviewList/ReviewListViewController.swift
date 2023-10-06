650b336e98cfd85af3e865baa2fdc23dd03d4091
seonyoung42 <sunwarrant@gmail.com>
2023-10-06 16:10:45 +0900

    [feat] Add BookReview

---
+++ BookReview/BookReview/Scene/ReviewList/ReviewListViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  BookReview
+//
+//  Created by 장선영 on 2023/10/05.
+//
+
+import UIKit
+import SnapKit
+
+final class ReviewListViewController: UIViewController {
+    
+    private lazy var presenter = ReviewListPresenter(viewController: self)
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = presenter
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        presenter.viewWillAppear()
+    }
+}
+
+extension ReviewListViewController: ReviewListProtocol {
+    func setupNavigationBar() {
+        navigationItem.title = "도서 리뷰"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let rightBarButton = UIBarButtonItem(barButtonSystemItem: .add,
+                                             target: self,
+                                             action: #selector(didTapRightBarButtonItem))
+        navigationItem.rightBarButtonItem = rightBarButton
+    }
+    
+    func setupViews() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func presentToReviewWriteViewController() {
+        let vc = UINavigationController(rootViewController: ReviewWriteViewController())
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+    }
+    
+    func reloadTableView() {
+        print("reloadTableView")
+        tableView.reloadData()
+    }
+}
+
+private extension ReviewListViewController {
+    @objc func didTapRightBarButtonItem() {
+        presenter.didTapRightBarButtonItem()
+    }
+}
