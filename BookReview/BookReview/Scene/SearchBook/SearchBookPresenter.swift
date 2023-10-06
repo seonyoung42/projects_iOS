e62baddac882811ca3a790eac2671ff2e9bfa854
seonyoung42 <sunwarrant@gmail.com>
2023-10-06 16:55:09 +0900

    [feat] make SearchBook UI

--- BookReview/BookReview/Scene/SearchBook/SearchBookPresenter.swift
+++ BookReview/BookReview/Scene/SearchBook/SearchBookPresenter.swift
@@ -5,13 +5,15 @@
 //  Created by 장선영 on 2023/10/06.
 //
 
+import UIKit
 import Foundation
 
 protocol SearchBookProtocol {
     func setupViews()
+    func dismiss()
 }
 
-final class SearchBookPresenter {
+final class SearchBookPresenter: NSObject {
     
     private let viewController: SearchBookProtocol
     
@@ -21,5 +23,28 @@ final class SearchBookPresenter {
     
     func viewDidLoad() {
         viewController.setupViews()
+    }
+}
+
+extension SearchBookPresenter: UISearchBarDelegate {
+    
+}
+
+extension SearchBookPresenter: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        viewController.dismiss()
+    }
+}
+
+extension SearchBookPresenter: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath)"
+        
+        return cell
     }
 }
--- BookReview/BookReview/Scene/SearchBook/SearchBookViewController.swift
+++ BookReview/BookReview/Scene/SearchBook/SearchBookViewController.swift
@@ -6,10 +6,18 @@
 //
 
 import UIKit
+import SnapKit
 
 final class SearchBookViewController: UIViewController {
     
     private lazy var presenter = SearchBookPresenter(viewController: self)
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = presenter
+        tableView.dataSource = presenter
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,5 +28,20 @@ final class SearchBookViewController: UIViewController {
 extension SearchBookViewController: SearchBookProtocol {
     func setupViews() {
         view.backgroundColor = .systemBackground
+        
+        let searchController = UISearchController()
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = presenter
+        navigationItem.searchController = searchController
+        
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func dismiss() {
+        dismiss(animated: true)
     }
 }
